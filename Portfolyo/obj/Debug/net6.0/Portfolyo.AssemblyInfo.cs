e2b3adceec78db19a2414bd3da64904d7fe3a54b
Mehmet Polat <mehwetpolat@gmail.com>
2025-03-14 17:35:00 +0300

    new template created and layers edited

--- Portfolyo/obj/Debug/net6.0/Portfolyo.AssemblyInfo.cs
+++ Portfolyo/obj/Debug/net6.0/Portfolyo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolyo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f2c4aa40721bd4f59b609a47ec071cce096db8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1328f394fd7e044175412b0db1365b1d2f1aa911")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolyo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolyo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

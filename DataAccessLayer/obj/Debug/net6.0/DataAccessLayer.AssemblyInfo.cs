ef9b1f039759d6d7523ff1b4dafdca21ec5313c7
Mehmet Polat <mehwetpolat@gmail.com>
2025-03-18 23:28:13 +0300

    project version control edited

--- DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7539a9aa2d98ae6496a9117463bc0e793430ed3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

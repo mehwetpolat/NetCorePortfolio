7539a9aa2d98ae6496a9117463bc0e793430ed3c
Mehmet Polat <mehwetpolat@gmail.com>
2025-03-18 23:19:24 +0300

    Revert "Revert "a""
    This reverts commit ce8659564d223e5e66143c8345a5b593d3ae0403.

--- EntityLayer/obj/Debug/net6.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Debug/net6.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817cb989b2e65a2b26a291a86f73bbee3758c157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

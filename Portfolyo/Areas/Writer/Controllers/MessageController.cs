39b591537749406e8400f179bf9d6a9195abb75a
Mehmet Polat <mehwetpolat@gmail.com>
2025-03-18 23:02:49 +0300

    Revert "controller listing and views codes and was written for writermessage"
    This reverts commit a28a597bc9625cf6e8f72bd7e9e978f8e231e685.
    
    "controller listing and views codes was written for writermessage"

--- Portfolyo/Areas/Writer/Controllers/MessageController.cs
+++ Portfolyo/Areas/Writer/Controllers/MessageController.cs
@@ -1,36 +1,13 @@
-﻿using BusinessLayer.Concrete;
-using DataAccessLayer.EntityFramework;
-using EntityLayer.Concrete;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Portfolyo.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class MessageController : Controller
     {
-        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
-        private readonly UserManager<WriterUser> _userManager;
-
-        public MessageController(UserManager<WriterUser> userManager)
+        public IActionResult Index()
         {
-            _userManager = userManager;
-        }
-
-        public async Task<IActionResult> ReceiverMessage(string p)
-        {
-            var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            p = values.Email;
-            var messageList = writerMessageManager.GetListReceiverMessage(p);
-            return View(messageList);
-        }
-
-        public async Task<IActionResult> SenderMessage(string p)
-        {
-            var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            p = values.Email;
-            var messageList = writerMessageManager.GetListSenderMessage(p);
-            return View(messageList);
+            return View();
         }
     }
 }

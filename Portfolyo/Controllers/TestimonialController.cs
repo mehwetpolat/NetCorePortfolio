5d56f9eff02eaf46e6e5ae76864e10e683e14def
Mehmet Polat <mehwetpolat@gmail.com>
2025-04-14 23:01:26 +0300

    testimonial page edited for admin page

---
+++ Portfolyo/Controllers/TestimonialController.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Migrations;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolyo.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = testimonialManager.TGetById(id);
+            testimonialManager.TDelete(value);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditTestimonial(int id)
+        {
+            var values = testimonialManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TUpdate(testimonial);
+            return RedirectToAction("Index");
+        }
+    }
+}

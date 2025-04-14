d089bf55f398221962416abf035ccac29923fb26
Mehmet Polat <mehwetpolat@gmail.com>
2025-04-14 22:57:28 +0300

    social media address has been dynamic

---
+++ Portfolyo/Controllers/SocialMediaController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portfolyo.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia socialMedia)
+        {
+            socialMedia.Status = true;
+            socialMediaManager.TAdd(socialMedia);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = socialMediaManager.TGetById(id);
+            socialMediaManager.TDelete(value);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+            var value = socialMediaManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            socialMediaManager.TUpdate(socialMedia);
+            return RedirectToAction("Index");
+
+        }
+    }
+}

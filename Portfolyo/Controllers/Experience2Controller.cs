e11d928a715b176068ab3b04222534b28081d5cb
Mehmet Polat <mehwetpolat@gmail.com>
2025-04-28 23:49:22 +0300

    ajax codes started for experience page

---
+++ Portfolyo/Controllers/Experience2Controller.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Portfolyo.Controllers
+{
+    public class Experience2Controller : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            var values = JsonConvert.SerializeObject(experience);
+            return Json(values);
+        }
+    }
+}
